7f519bcfc96bc8cab28a18a9998340cabbf5639c
Priyanshu Vishwakarma <priyanshu.vishwakarma.code@gmail.com>
2026-07-09 20:41:33 +0530

    Time: 377 ms (48.83%), Space: 46.2 MB (63.66%) - LeetHub

---
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int getdis(int p1 , int p2 , vector<vector<int>>&points){
+        return abs(points[p1][0] - points[p2][0]) + abs(points[p1][1] - points[p2][1]);
+    }
+
+    int minCostConnectPoints(vector<vector<int>>& points) {
+        int n = points.size();
+        priority_queue< pair<int,int> , vector<pair<int,int>> , greater<pair<int,int>> > pq;
+        vector<bool>visited(n,false);
+        int mincost = 0;
+
+        pq.push({0,0}); // weight , element
+        while(!pq.empty()){
+            auto p = pq.top();
+            int weight = p.first;
+            int node = p.second;
+            pq.pop();
+
+            if(visited[node]) continue;
+            visited[node] = true;
+
+            mincost += weight;
+
+            for(int i = 0 ; i<n ; i++){
+
+                if(!visited[i]){
+                    int distance = getdis(node, i, points);
+                    pq.push({distance,i});
+                } 
+            }
+        }
+
+        return mincost;
+    }
+};
