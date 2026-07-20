ead3167da065d3803958d6b45bfde8c0673f850a
Priyanshu Vishwakarma <priyanshu.vishwakarma.code@gmail.com>
2026-07-20 09:23:22 +0530

    Time: 0 ms (100%), Space: 8.5 MB (52.94%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        vector<int>dp(n+1, -1);
-        return recursion(n, dp);
-    }
-
-    int recursion(int n, vector<int>&dp){
-        if(n<=2) return dp[n] = n;
-
-        if(dp[n]!=-1) return dp[n];
-
-        return dp[n] = recursion(n-1, dp) + recursion(n-2, dp);
+        vector<int>dp(n+1 , -1);
+        if(n == 1 || n == 2) return n;
+        else{
+            dp[1] = 1, dp[2] = 2, dp[0] = 0;
+            for(int i = 3 ; i<=n ; i++){
+                dp[i] = dp[i-1] + dp[i-2];
+            }
+        }
+        return dp[n];
     }
 };
