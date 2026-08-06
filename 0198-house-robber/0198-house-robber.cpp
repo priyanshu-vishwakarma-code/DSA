53878277981d453c2c5ff3a1649eb3bc84f19ff0
Priyanshu Vishwakarma <priyanshu.vishwakarma.code@gmail.com>
2026-08-06 08:48:17 +0530

    Time: 0 ms (100%), Space: 10.6 MB (65.08%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -22,6 +22,18 @@ public:
     int rob(vector<int>& nums) {
         int n = nums.size();
         vector<int>dp(n, -1);
-        return recursion(0, dp, nums);
+        // return recursion(0, dp, nums);     // the recursive + memoriztaion method
+
+        // THE TABULATION METHOD
+        if(n==1) return nums[0];
+        else if(n==2) return max(nums[0],nums[1]);
+        dp[0] = nums[0], dp[1] = max(nums[0],nums[1]);
+        for(int i = 2 ; i<n ; i++){
+            int rob = dp[i-2] + nums[i];
+            int skip = dp[i-1];
+            if(rob > skip) dp[i] = rob;
+            else dp[i] = skip;
+        }
+        return dp[n-1];
     }
 };
