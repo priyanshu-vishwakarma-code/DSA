04322b1f51b70bd2b6442fd23b4b67c96f07d3c7
Priyanshu Vishwakarma <priyanshu.vishwakarma.code@gmail.com>
2026-08-06 09:01:27 +0530

    Time: 0 ms (100%), Space: 10.9 MB (6.17%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -25,15 +25,29 @@ public:
         // return recursion(0, dp, nums);     // the recursive + memoriztaion method
 
         // THE TABULATION METHOD
+        // if(n==1) return nums[0];
+        // else if(n==2) return max(nums[0],nums[1]);
+        // dp[0] = nums[0], dp[1] = max(nums[0],nums[1]);
+        // for(int i = 2 ; i<n ; i++){
+        //     int rob = dp[i-2] + nums[i];
+        //     int skip = dp[i-1];
+        //     if(rob > skip) dp[i] = rob;
+        //     else dp[i] = skip;
+        // }
+        // return dp[n-1];
+
+        // TABULATION + SPACE OPTIMIZATION
         if(n==1) return nums[0];
         else if(n==2) return max(nums[0],nums[1]);
-        dp[0] = nums[0], dp[1] = max(nums[0],nums[1]);
+        int x = nums[0], y = max(nums[0],nums[1]);
         for(int i = 2 ; i<n ; i++){
-            int rob = dp[i-2] + nums[i];
-            int skip = dp[i-1];
-            if(rob > skip) dp[i] = rob;
-            else dp[i] = skip;
+            int rob = x + nums[i], skip = y;
+            if(rob > skip){
+                x = y;
+                y = rob;
+            }
+            else x = skip, y = skip;
         }
-        return dp[n-1];
+        return y;
     }
 };
