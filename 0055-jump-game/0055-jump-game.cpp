0c27becf4c0695b5c357dd5f2a3e8ff28d9a6233
Priyanshu Vishwakarma <priyanshu.vishwakarma.code@gmail.com>
2026-08-12 07:01:54 +0530

    Time: 250 ms (13.26%), Space: 55.8 MB (5.85%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool recursion(int i, vector<int>&dp, vector<int>&nums){
+        if(i == nums.size()-1) return true;
+        if(i >= nums.size()) return false;
+
+        if(dp[i] != -1) return dp[i];
+
+        int jump = nums[i];
+        while(jump > 0){
+            if(recursion(i+jump, dp, nums)) return dp[i] = true;
+            jump--;
+        }
+
+        return dp[i] = false;
+    }
+
+    bool canJump(vector<int>& nums) {
+        int n = nums.size();
+        vector<int>dp(n, -1);
+        return recursion(0, dp, nums);
+    }
+};
