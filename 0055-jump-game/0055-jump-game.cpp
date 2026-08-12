73e936f56943f56299d020d4297cc8f0fcbb0aed
Priyanshu Vishwakarma <priyanshu.vishwakarma.code@gmail.com>
2026-08-12 07:36:17 +0530

    Time: 0 ms (100%), Space: 52.3 MB (58.34%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -17,7 +17,17 @@ public:
 
     bool canJump(vector<int>& nums) {
         int n = nums.size();
-        vector<int>dp(n, -1);
-        return recursion(0, dp, nums);
+
+        // RECURSION + MEMORIZATION
+        // vector<int>dp(n, -1);
+        // return recursion(0, dp, nums);
+
+        // GREDDY SOLUTION
+        int farthest = 0;
+        for(int i = 0 ; i<n ; i++){
+            if(i > farthest) return false;
+            farthest = max(farthest, i + nums[i]);
+        }
+        return true;
     }
 };
