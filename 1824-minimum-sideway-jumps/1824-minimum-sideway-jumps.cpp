c3e15ec5bd75131026f9cea7089d61b36796494a
Priyanshu Vishwakarma <priyanshu.vishwakarma.code@gmail.com>
2026-09-09 08:30:50 +0530

    Time: 23 ms (95.03%), Space: 191.8 MB (84.65%) - LeetHub

--- 1824-minimum-sideway-jumps/1824-minimum-sideway-jumps.cpp
+++ 1824-minimum-sideway-jumps/1824-minimum-sideway-jumps.cpp
@@ -22,9 +22,32 @@ public:
     }
 
     int minSideJumps(vector<int>& obstacles) {
+        // RECURSION + MEMORIZATION
+        // int n = obstacles.size();
+        // vector<vector<int>>dp(n, (vector<int>(4,-1)));
+        // return recursion(0, 2, n, dp, obstacles);  // curr point, curr lane, n, obsticles
+        
+        // TABULATION
         int n = obstacles.size();
-        vector<vector<int>>dp(n, (vector<int>(4,-1)));
-        int ans = recursion(0, 2, n, dp, obstacles);  // curr point, curr lane, n, obsticles
-        return ans;
+        const int INF = 1e9;
+        vector<int>dp(4,INF);
+        dp[1] = 1;
+        dp[2] = 0;
+        dp[3] = 1;
+
+        for(int curr = 1 ; curr < n ; curr++){
+            int rock = obstacles[curr];
+
+            if(rock != 0) dp[rock] = INF;
+
+            int minimum = min(dp[1], min(dp[2], dp[3]));
+            for(int lane = 1 ; lane<=3 ; lane++){
+                if(rock != lane){
+                    dp[lane] = min(dp[lane], minimum+1);
+                }
+            }
+        }
+
+        return min(dp[1], min(dp[2], dp[3]));
     }
 };
